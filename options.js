4111f93f040540cdfd4a28a36a57267cbb19cc1b
Andrew Dassonville <andrewda@users.noreply.github.com>
2015-09-08 23:22:30 -0700

    Create options.js

---
+++ options.js
@@ -0,0 +1,14 @@
+{
+    "mysql": {
+        "host": "",
+        "user": "",
+        "port": 3306,
+        "password": "",
+        "database": ""
+    },
+    "errors": {
+        "missing_params": "non-existant parameter(s)",
+        "invalid_key": "insufficient privileges",
+        "unknown_item": "unknown item"
+    }
+}

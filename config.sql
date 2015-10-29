fc3d781188187adf54f406deed4931ad4b6f6943
Andrew Dassonville <andrewda@users.noreply.github.com>
2015-10-28 23:25:35 -0700

    Add comments

--- config.sql
+++ config.sql
@@ -1,6 +1,15 @@
+/*
+ * This is the MySQL config script for http://github.com/andrewda/CSGOItemDB.com
+ * It will create 3 tables, `prices`, `price_history`, and `keys`
+ */
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+/*
+ * `prices` stores the current item prices
+ */
+ 
 CREATE TABLE IF NOT EXISTS `prices` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item` text NOT NULL,
@@ -9,19 +18,27 @@ CREATE TABLE IF NOT EXISTS `prices` (
   `avg_month_price` text NOT NULL,
   `lastupdate` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6613 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
 
+/*
+ * `price_history` stores all prices within the last month (by default)
+ */
+ 
 CREATE TABLE IF NOT EXISTS `price_history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item` text NOT NULL,
   `price` text NOT NULL,
   `time` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2352 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
 
+/*
+ * `keys` stores the current API keys
+ */
+ 
 CREATE TABLE IF NOT EXISTS `keys` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `key` text NOT NULL,
   `premium` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
